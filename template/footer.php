1f272be4b12a960b9dfb627fb0a3ac6b2ead1d71
pyatkoff <pavel@pyatkoff.com>
2026-08-25 10:28:17 +0200

    Add standalone test footer

---
+++ template/footer.php
@@ -0,0 +1,7 @@
+</main>
+<footer class="testFooter">
+    <div class="testFooterInner">Тестовая версия поиска туров</div>
+</footer>
+<script src="/poisk-turov-test/template/search/script.js"></script>
+</body>
+</html>
