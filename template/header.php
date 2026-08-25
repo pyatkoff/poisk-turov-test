25925747595cdee6669940901aab659501a9e14c
pyatkoff <pavel@pyatkoff.com>
2026-08-25 13:02:07 +0200

    Bump compact mobile form assets

--- template/header.php
+++ template/header.php
@@ -12,11 +12,11 @@ $APPLICATION->AddHeadString('<link rel="stylesheet" href="/poisk-turov-test/temp
 require __DIR__ . '/../site-template/header.php';
 ?>
 <link rel="stylesheet" href="/poisk-turov-test/template/form-overrides.css?v=2">
-<link rel="stylesheet" href="/poisk-turov-test/template/form-layout-fixes.css?v=4">
+<link rel="stylesheet" href="/poisk-turov-test/template/form-layout-fixes.css?v=5">
 <link rel="stylesheet" href="/poisk-turov-test/template/card-overrides.css?v=3">
 <link rel="stylesheet" href="/poisk-turov-test/template/results-controls.css?v=1">
 <link rel="stylesheet" href="/poisk-turov-test/template/hotel-details-overrides.css?v=1">
 <link rel="stylesheet" href="/poisk-turov-test/template/tour-final-overrides.css?v=1">
 <link rel="stylesheet" href="/poisk-turov-test/template/mobile-critical-fixes.css?v=2">
-<script src="/poisk-turov-test/template/layout-fixes.js?v=5" defer></script>
+<script src="/poisk-turov-test/template/layout-fixes.js?v=6" defer></script>
 <script src="/poisk-turov-test/template/results-controls.js?v=1" defer></script>
