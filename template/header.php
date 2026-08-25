825e261d0ad1c30cae48ceeaf659263f13fe2885
pyatkoff <pavel@pyatkoff.com>
2026-08-25 10:28:08 +0200

    Add standalone test header

---
+++ template/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Поиск туров — тест</title>
+    <link rel="icon" href="/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&subset=cyrillic-ext" rel="stylesheet">
+    <link href="/css/css_new.css" rel="stylesheet">
+    <link href="/css/datepicker/datepicker.css" rel="stylesheet">
+    <link href="/poisk-turov-test/template/search/style.css" rel="stylesheet">
+    <link href="/poisk-turov-test/template/search/slick/slick.css" rel="stylesheet">
+    <link href="/poisk-turov-test/template/style.css" rel="stylesheet">
+
+    <script src="/js/jquery-3.3.1.min.js"></script>
+    <script src="/js/datepicker/datepicker.min.js"></script>
+    <script src="/js/js.cookie-2.2.0.min.js"></script>
+    <script src="/js/jquery.mask.min.js"></script>
+    <script src="/js/slick/slick.min.js"></script>
+    <script src="/js/script.js?v10"></script>
+    <script>
+      window.ym = window.ym || function(){ (window.ym.a = window.ym.a || []).push(arguments); };
+    </script>
+</head>
+<body class="tour-search-test">
+<header class="testHeader">
+    <div class="testHeaderInner">
+        <a class="testLogo" href="/poisk-turov-test/">ANYTOUR</a>
+        <span class="testBadge">тестовый поиск</span>
+    </div>
+</header>
+<main class="testMain">
