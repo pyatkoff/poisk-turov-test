ae1ed5937cd7af5fd77f161b5bd17dddc071252b
pyatkoff <pavel@pyatkoff.com>
2026-08-25 10:27:54 +0200

    Build standalone tour search page shell

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Поиск туров");
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/site_conf.php");
+require_once(__DIR__."/tv_api/tvapi.php");
 
-$dir = $APPLICATION->GetCurDir();
-$baseDir = "/poisk-turov-test/";
-$code = "";
-if ($dir !== $baseDir)
-{
-    $code = str_replace([$baseDir, "/"], "", $dir);
+$dataFilter = ["COUNTRY" => 4];
+if (!empty($params["TV_CITY"])) {
+    $dataFilter["FROM"] = $params["TV_CITY"];
 }
-?>
+if (isset($_GET["from"])) {
+    $dataFilter["FROM"] = (int)$_GET["from"];
+}
+if (isset($_GET["country"])) {
+    $dataFilter["COUNTRY"] = (int)$_GET["country"];
+}
 
-<div data-marquiz-id="64a2e3becefb38002582445a"></div>
-<script>(function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Button', {id: '64a2e3becefb38002582445a', buttonText: 'Хотите подберем Вам тур?', bgColor: '#d34085', textColor: '#fff', rounded: true, shadow: 'rgba(211, 64, 133, 0.5)', blicked: true})</script>
+$arResult = [
+    "AUTO_START" => "n",
+    "HOTEL_MODE" => false,
+    "HOTEL" => "",
+    "COUNTRY" => $dataFilter["COUNTRY"],
+    "FORM_PARAMS" => TvApi::prepareForm($dataFilter),
+];
 
-<?$APPLICATION->IncludeComponent(
-    "rhat.search:form",
-    "form",
-    Array(
-        "FROM" => $params["TV_CITY"],
-        "COUNTRY" => 4,
-        "CACHE_TYPE" => "N",
-        "FORM_CODE" => $code
-    ),
-    false
-);?>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$templateFolder = "/poisk-turov-test/template/search";
+require __DIR__."/template/header.php";
+require __DIR__."/template/search/template.php";
+require __DIR__."/template/footer.php";
