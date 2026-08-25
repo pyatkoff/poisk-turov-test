ae5e16315aac0a2e41a4cc8abf175f9c8082e10c
pyatkoff <pavel@pyatkoff.com>
2026-08-25 08:31:08 +0200

    Fix test search base path and clean index

--- index.php
+++ index.php
@@ -1,38 +1,29 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Поиск туров");
+
 $dir = $APPLICATION->GetCurDir();
+$baseDir = "/poisk-turov-test/";
 $code = "";
-if($dir!="/poisk-turov/")
+if ($dir !== $baseDir)
 {
-	$code = str_replace(["/poisk-turov/","/"],"",$dir);
+    $code = str_replace([$baseDir, "/"], "", $dir);
 }
-
-
 ?>
 
 <div data-marquiz-id="64a2e3becefb38002582445a"></div>
 <script>(function(t, p) {window.Marquiz ? Marquiz.add([t, p]) : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})('Button', {id: '64a2e3becefb38002582445a', buttonText: 'Хотите подберем Вам тур?', bgColor: '#d34085', textColor: '#fff', rounded: true, shadow: 'rgba(211, 64, 133, 0.5)', blicked: true})</script>
 
-<?/*?>
-<div style="margin:0 auto; max-width:1020px; padding:20px;">
-<div class="tv-search-form tv-moduleid-175393" tv-departure="<?=$params["TV_CITY"]?>"></div>
-<sc ript type="text/javascript" src="//tourvisor.ru/module/init.js"></sc ript>
-</div>
-<?*/?>
-<??>
 <?$APPLICATION->IncludeComponent(
-	"rhat.search:form", "form",
-	Array(
-		"FROM" =>$params["TV_CITY"],
-		"COUNTRY" => 	4,
-		"CACHE_TYPE" => "N",
-		"FORM_CODE"=>$code
-	),
-	false
-);?> 
-<??>
-
-
+    "rhat.search:form",
+    "form",
+    Array(
+        "FROM" => $params["TV_CITY"],
+        "COUNTRY" => 4,
+        "CACHE_TYPE" => "N",
+        "FORM_CODE" => $code
+    ),
+    false
+);?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
