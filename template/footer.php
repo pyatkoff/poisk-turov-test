fb2c028c3b93b27e94ec965acb27f8b39ef6bb77
pyatkoff <pavel@pyatkoff.com>
2026-08-27 08:24:41 +0200

    Delete template directory

--- template/footer.php
+++
@@ -1,5 +0,0 @@
-<?php
-/** Test-only footer: local search JS + frozen site footer. */
-?>
-<script src="/poisk-turov-test/template/search/script.js"></script>
-<?php require __DIR__ . '/../site-template/footer.php'; ?>
