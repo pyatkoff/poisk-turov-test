3d244646b0ec268e34d0e8db2f6b11597b64aa34
pyatkoff <pavel@pyatkoff.com>
2026-08-29 02:04:03 +0300

    Fix standalone privacy consent destination
    * fix: keep standalone privacy consent link reachable
    
    * test: guard standalone privacy destination

--- v2/privacy-config.php
+++ v2/privacy-config.php
@@ -1,5 +1,10 @@
 <?php
 function v2_privacy_url(): string
 {
+    $host = strtolower(trim((string)($_SERVER['HTTP_HOST'] ?? '')));
+    $host = preg_replace('/:\d+$/', '', $host);
+    if ($host === 'anytoour.ru') {
+        return 'https://anytour.online/politika-konfidentsialnosti/';
+    }
     return '/politika-konfidentsialnosti/';
 }
