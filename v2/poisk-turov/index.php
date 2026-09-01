75fed683fb4f752d7c9116d473eba2e0261324ea
pyatkoff <pavel@pyatkoff.com>
2026-09-01 22:51:01 +0200

    Preview: route Search 2.0 assets under preview base

--- v2/poisk-turov/index.php
+++ v2/poisk-turov/index.php
@@ -1,13 +1,16 @@
 <?php
 $host = strtolower((string)($_SERVER['HTTP_HOST'] ?? ''));
+$script = str_replace('\\', '/', (string)($_SERVER['SCRIPT_NAME'] ?? ''));
 $isStandalone = $host === 'anytoour.ru' || $host === 'www.anytoour.ru' || str_starts_with($host, 'anytoour.ru:') || str_starts_with($host, 'www.anytoour.ru:');
+$isSearch2Preview = str_starts_with($script, '/_preview/search2/');
 if (!defined('V2_FORCE_SEARCH_PAGE')) define('V2_FORCE_SEARCH_PAGE', true);
-if (!defined('V2_CANONICAL_PATH')) define('V2_CANONICAL_PATH', '/poisk-turov/');
+if (!defined('V2_CANONICAL_PATH')) define('V2_CANONICAL_PATH', $isSearch2Preview ? '/_preview/search2/poisk-turov/' : '/poisk-turov/');
 if (!defined('V2_PUBLIC_BASE_PATH')) {
-    if ($isStandalone) {
+    if ($isSearch2Preview) {
+        define('V2_PUBLIC_BASE_PATH', '/_preview/search2');
+    } elseif ($isStandalone) {
         define('V2_PUBLIC_BASE_PATH', '');
     } else {
-        $script = str_replace('\\', '/', (string)($_SERVER['SCRIPT_NAME'] ?? ''));
         $base = dirname(dirname($script));
         define('V2_PUBLIC_BASE_PATH', $base === '/' ? '' : $base);
     }
