45b06abe928ddcab8679c103fef92a5f816ff43e
pyatkoff <pavel@pyatkoff.com>
2026-09-02 00:02:46 +0200

    Search2 preview: mark isolated preview runtime

--- v2/poisk-turov/index.php
+++ v2/poisk-turov/index.php
@@ -4,6 +4,7 @@ $script = str_replace('\\', '/', (string)($_SERVER['SCRIPT_NAME'] ?? ''));
 $isStandalone = $host === 'anytoour.ru' || $host === 'www.anytoour.ru' || str_starts_with($host, 'anytoour.ru:') || str_starts_with($host, 'www.anytoour.ru:');
 $isSearch2Preview = str_starts_with($script, '/_preview/search2/');
 if (!defined('V2_FORCE_SEARCH_PAGE')) define('V2_FORCE_SEARCH_PAGE', true);
+if (!defined('V2_SEARCH2_PREVIEW')) define('V2_SEARCH2_PREVIEW', $isSearch2Preview);
 if (!defined('V2_CANONICAL_PATH')) define('V2_CANONICAL_PATH', $isSearch2Preview ? '/_preview/search2/poisk-turov/' : '/poisk-turov/');
 if (!defined('V2_PUBLIC_BASE_PATH')) {
     if ($isSearch2Preview) {
