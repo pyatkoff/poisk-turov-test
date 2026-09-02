0b16669503930bc2b241ee0dc880f45ce3f6e631
pyatkoff <pavel@pyatkoff.com>
2026-09-02 17:11:51 +0200

    Remove legacy privacy host dependency

--- v2/privacy-config.php
+++ v2/privacy-config.php
@@ -1,10 +1,5 @@
 <?php
 function v2_privacy_url(): string
 {
-    $host = strtolower(trim((string)($_SERVER['HTTP_HOST'] ?? '')));
-    $host = preg_replace('/:\d+$/', '', $host);
-    if ($host === 'anytoour.ru') {
-        return 'https://anytour.online/politika-konfidentsialnosti/';
-    }
     return '/politika-konfidentsialnosti/';
 }
