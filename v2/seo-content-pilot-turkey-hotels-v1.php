915f408b1861aad14291e9aa4be192536c4938ef
pyatkoff <pavel@pyatkoff.com>
2026-09-02 05:42:37 +0200

    SEO2: extend Turkey hotel review manifest

--- v2/seo-content-pilot-turkey-hotels-v1.php
+++ v2/seo-content-pilot-turkey-hotels-v1.php
@@ -9,6 +9,9 @@ function v2_seo_turkey_hotel_manifest(): array
         ['file'=>'seo-content-pilot-turkey-first-class-v1.php','function'=>'v2_seo_content_pilot_turkey_first_class','label'=>'Туры в FIRST CLASS HOTEL','href'=>'/country/turkey/hotel/first-class-hotel-1188/'],
         ['file'=>'seo-content-pilot-turkey-bodrum-beach-resort-v1.php','function'=>'v2_seo_content_pilot_turkey_bodrum_beach_resort','label'=>'Туры в BODRUM BEACH RESORT','href'=>'/country/turkey/hotel/bodrum-beach-resort-9250/'],
         ['file'=>'seo-content-pilot-turkey-club-hotel-sunbel-v1.php','function'=>'v2_seo_content_pilot_turkey_club_hotel_sunbel','label'=>'Туры в CLUB HOTEL SUNBEL','href'=>'/country/turkey/hotel/club-hotel-sunbel-1102/'],
+        ['file'=>'seo-content-pilot-turkey-dedeman-kemer-v1.php','function'=>'v2_seo_content_pilot_turkey_dedeman_kemer','label'=>'Туры в DEDEMAN KEMER RESORT','href'=>'/country/turkey/hotel/dedeman-kemer-resort-ex-club-jovia-kemer-28476/'],
+        ['file'=>'seo-content-pilot-turkey-concordia-celes-v1.php','function'=>'v2_seo_content_pilot_turkey_concordia_celes','label'=>'Туры в CONCORDIA CELES HOTEL','href'=>'/country/turkey/hotel/concordia-celes-hotel-1134/'],
+        ['file'=>'seo-content-pilot-turkey-casa-fora-v1.php','function'=>'v2_seo_content_pilot_turkey_casa_fora','label'=>'Туры в CASA FORA BEACH RESORT BY MIRAMOR','href'=>'/country/turkey/hotel/casa-fora-beach-resort-by-miramor-90704/'],
     ];
 }
 
