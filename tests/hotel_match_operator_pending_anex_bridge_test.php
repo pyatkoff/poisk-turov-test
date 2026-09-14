187565a2fca4af197b2f83fe207774dbf6bd8c70
pyatkoff <pavel@pyatkoff.com>
2026-09-15 00:08:26 +0200

    MATCH: normalize numeric Andromeda ids in offline bridge test

--- tests/hotel_match_operator_pending_anex_bridge_test.php
+++ tests/hotel_match_operator_pending_anex_bridge_test.php
@@ -4,7 +4,7 @@ define('OPB_LIBRARY_ONLY',true);
 require_once __DIR__.'/../scripts/diagnostics/hotel_match_operator_pending_anex_bridge.php';
 $n=0;function t(bool $v):void{global $n;if(!$v)throw new RuntimeException('assertion_'.($n+1));$n++;}
 $e=['schema'=>'operator-original-price-bridge/1','source'=>['id'=>'4158','operator_key'=>'5','name'=>'Barcelo Tiran Sharm'],'provider_bridges'=>[['operator_key'=>'5','native_hotel_id'=>'4158','andromeda_hotel_id'=>'177152','country_id'=>1,'hotel_name'=>'Barcelo Tiran Sharm Hotel Sharm El Sheikh','original_name'=>'Barcelo Tiran Sharm Hotel Sharm El Sheikh','is_operator_hotel_key'=>false,'action'=>'price']]];
-$b=opb_bridge($e,'4158');t($b['ok']);t($b['andromeda_ids']===['177152']);t($b['country_id']===1);
+$b=opb_bridge($e,'4158');t($b['ok']);t(array_map('strval',$b['andromeda_ids'])===['177152']);t($b['country_id']===1);
 $x=$e;$x['source']['operator_key']='315';t(!opb_bridge($x,'4158')['ok']);
 $x=$e;$x['provider_bridges'][0]['operator_key']='342';t(!opb_bridge($x,'4158')['ok']);
 $x=$e;$x['provider_bridges'][0]['native_hotel_id']='999';t(!opb_bridge($x,'4158')['ok']);
