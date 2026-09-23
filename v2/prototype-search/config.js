067a6232d59479bacae80b582b4c2e3d1e08cf40
pyatkoff <pavel@pyatkoff.com>
2026-09-23 21:50:06 +0200

    SEARCH: expose Andromeda quote endpoint in prototype config

--- v2/prototype-search/config.js
+++ v2/prototype-search/config.js
@@ -1,2 +1,2 @@
 'use strict';
-window.V2_CONFIG={api:'/api-v2.php',leadApi:'../preview-lead-disabled.php',andromedaApi:'/_preview/search3-anex-candidate/api-andromeda-search3-preview.php',anexApi:'/_preview/search3-anex-candidate/api-anex-search3-preview.php'};
+window.V2_CONFIG={api:'/api-v2.php',leadApi:'../preview-lead-disabled.php',andromedaApi:'/_preview/search3-anex-candidate/api-andromeda-search3-preview.php',andromedaQuoteApi:'/_preview/search3-anex-candidate/api-andromeda-quote-preview.php',anexApi:'/_preview/search3-anex-candidate/api-anex-search3-preview.php'};
