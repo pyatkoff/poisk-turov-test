dde9e19bdc4ba17a5e09515dbd40c26325f7d5bc
pyatkoff <pavel@pyatkoff.com>
2026-09-25 01:01:22 +0200

    MATCH: commit wave4 CURRENT writer-ready identities

---
+++ tests/hotel_match_samo_business_live30_common4_wave4_writer_v13_w1_test.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/../scripts/diagnostics/hotel_match_samo_business_live30_common4_wave4_writer_v13_w1.php';
+sbw5_self_test();
+echo "MATCH_SAMO_BUSINESS_LIVE30_WAVE4_WRITER_V13_W1_TEST_OK\n";
