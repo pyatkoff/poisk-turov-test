a86a8809608383445e219d91c6bcf3a764ccb7b2
pyatkoff <pavel@pyatkoff.com>
2026-09-25 11:42:43 +0200

    MATCH: test cross-namespace equivalence audit

---
+++ tests/hotel_match_common4_crossnamespace_equivalence_v19_test.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/../scripts/diagnostics/hotel_match_common4_crossnamespace_equivalence_v19.php';
+cne19_self_test();
+echo "MATCH_COMMON4_CROSSNAMESPACE_EQUIVALENCE_V19_TEST_OK\n";
