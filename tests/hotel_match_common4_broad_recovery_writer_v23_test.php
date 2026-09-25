cde311b1c23e0d263bfe124ebab4661f42a556d5
pyatkoff <pavel@pyatkoff.com>
2026-09-25 12:30:38 +0200

    MATCH: commit broad recovery CURRENT identities

---
+++ tests/hotel_match_common4_broad_recovery_writer_v23_test.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/../scripts/diagnostics/hotel_match_common4_broad_recovery_writer_v23.php';
+sbw5_self_test();
+echo "MATCH_COMMON4_BROAD_RECOVERY_WRITER_V23_TEST_OK\n";
