c9954734a190da6bf8a47f1d9a2915cfa6e42c0e
pyatkoff <pavel@pyatkoff.com>
2026-09-25 11:54:06 +0200

    MATCH: test broad recovery acquisition

---
+++ tests/hotel_match_common4_broad_recovery_acquire_v21_test.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/../scripts/diagnostics/hotel_match_common4_broad_recovery_acquire_v21.php';
+c4b21_self_test();
+echo "MATCH_COMMON4_BROAD_RECOVERY_ACQUIRE_V21_TEST_OK\n";
