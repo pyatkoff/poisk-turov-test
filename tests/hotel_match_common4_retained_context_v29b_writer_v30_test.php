6f44ddfb27e263f8f4fafae6cd2af31d24af3c7b
pyatkoff <pavel@pyatkoff.com>
2026-09-25 18:02:56 +0200

    MATCH retained-context v29b writer v30 test

---
+++ tests/hotel_match_common4_retained_context_v29b_writer_v30_test.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/../scripts/diagnostics/hotel_match_common4_retained_context_v29b_writer_v30.php';
+sbw5_self_test();
+echo "MATCH_COMMON4_RETAINED_CONTEXT_V29B_WRITER_V30_TEST_OK\n";
