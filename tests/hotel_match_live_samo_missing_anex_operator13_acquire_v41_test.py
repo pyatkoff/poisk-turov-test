e7c5d9b2be0d9e3ff863ae801c7ac8c90e453a53
pyatkoff <pavel@pyatkoff.com>
2026-09-25 20:05:08 +0200

    MATCH: fix v41b test line break

--- tests/hotel_match_live_samo_missing_anex_operator13_acquire_v41_test.py
+++ tests/hotel_match_live_samo_missing_anex_operator13_acquire_v41_test.py
@@ -6,7 +6,8 @@ spec=importlib.util.spec_from_file_location('v41',p)
 m=importlib.util.module_from_spec(spec);spec.loader.exec_module(m)
 
 assert m.OP=='hotel-match-live-samo-missing-anex-operator13-acquire-1971-20260925-v41b'
-assert m.ACCOUNT=='TOURVISOR_ANEX_JWT'\nassert m.ACCOUNT_LEDGER=='tourvisor-anex'
+assert m.ACCOUNT=='TOURVISOR_ANEX_JWT'
+assert m.ACCOUNT_LEDGER=='tourvisor-anex'
 assert m.DAILY_LIMIT==3000 and m.CALL_CAP==500 and m.MAX_BATCHES==50
 x=m.link_projection({'operatorLink':'https://agent.anextour.ru/x?HOTELLIST=5844'})
 assert x['link_state']=='captured_single_native' and x['positive_native_candidates']==[5844]
